7ad6a2324826ba0d5987b012341a879805498489
asm-shaikat <asmshaikat1952@gmail.com>
2024-10-26 21:57:53 +0600

    image bug solved

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -8,21 +8,23 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
 const HeroSlider = () => {
-    const sliderImage = [
-        {
-          src: 'https://i.ibb.co.com/jzy0hnn/luxury-real-estate.jpg',
-          alt: 'Image 2'  
-        },
-        {
-          src: 'https://i.ibb.co.com/8s3yw3t/What-are-real-estate-comps.jpg',
-          alt: 'Image 1'
-          
-        },
-        {
-          src: 'https://i.ibb.co.com/bWC80NT/2a65e333b3e450407ceb50e3653bf030-cc-ft-960.jpg',
-          alt: 'Image 3'
-        }
-    ];
+  const sliderImage = [
+    {
+      src: 'https://i.ibb.co.com/bWC80NT/2a65e333b3e450407ceb50e3653bf030-cc-ft-960.jpg',
+      alt: 'Image 1',
+      link: 'https://ibb.co.com/w0PxkpV',
+    },
+    {
+      src: 'https://i.ibb.co.com/jzy0hnn/luxury-real-estate.jpg',
+      alt: 'Image 2',
+      link: 'https://ibb.co.com/0KC4qHH',
+    },
+    {
+      src: 'https://i.ibb.co.com/8s3yw3t/What-are-real-estate-comps.jpg',
+      alt: 'Image 3',
+      link: 'https://ibb.co.com/54DQ3Dw',
+    },
+  ];
 
     return (
       <div className="w-full px-4 lg:px-0"> 
